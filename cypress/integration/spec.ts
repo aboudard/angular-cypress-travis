fdf5950659c8bfc5f9d0f6c7cb8d3c933effd4d5
Alain Boudard <coco.boudard@gmail.com>
2019-03-15 11:02:40 +0100

    init

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,12 @@
+import { navigateTo, getGreeting } from './../support/po';
+
+describe('Hello Angular', () => {
+	beforeEach(navigateTo);
+	it('should display welcome message', () => {
+		getGreeting().contains('Welcome to appv7!');
+  });
+
+  it('has 3 links', () => {
+    cy.get('app-root li a').should('have.length', 3);
+  });
+});
