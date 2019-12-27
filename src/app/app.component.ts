8a414a8f18d09be294b5fd54e0f6537b17d40577
Alain BOUDARD <a12666@orleanssrv.domsma>
2019-12-27 15:36:09 +0100

    metas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from './shared/todo.service';
 import { Todo } from './model/todo';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,11 @@ export class AppComponent implements OnInit {
   title = 'appv7';
 
   items: Todo[] = [];
-  constructor(public todoService: TodoService) {}
+  constructor(public todoService: TodoService, private meta: Meta) {
+    this.meta.addTag({ name: 'description', content: 'How to build Angular app with Travis CI and Sonar Cloud' });
+    this.meta.addTag({ name: 'author', content: 'Alain Boudard' });
+    this.meta.addTag({ name: 'twitter:site', content: '@aboudard' });
+  }
 
   ngOnInit() {
     this.todoService
